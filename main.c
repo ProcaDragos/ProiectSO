5d6426452bb0fb97e74bd50db8958f91c1735138
ProcaDragos <163131035+ProcaDragos@users.noreply.github.com>
2025-04-06 21:04:27 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+typedef struct {
+    int tresureid;
+    char name[40];
+    float x;
+    float y;
+    char text[100];
+    int val;
+}tr;
+int main( int argc,char *argv[]) {
+    
+    return 0;
+}
